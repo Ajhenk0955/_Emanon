ebed0f6e8312a7e7810c5fb94e1541dc602840c8
AHenk <ajhenk0955@eagle.fgcu.edu>
2015-05-29 10:57:50 -0400

    MainMenuController Skeleton

---
+++ Client/src/mainMenu/MainMenuController.java
@@ -0,0 +1,55 @@
+package applicationV2;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class MainMenuController {
+
+    @FXML
+    private Button searchButton;
+
+    @FXML
+    private TextField searchTerm;
+
+    @FXML
+    private Button quickSearch;
+
+    @FXML
+    private Button edit_UserProfile;
+
+    @FXML
+    private Button createPatientButton;
+
+    @FXML
+    private Button logoutButton;
+
+    @FXML
+
+
+    @FXML
+    void handleSearchButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleQuickSearchButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleUserProfile(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleAddPatientButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleMMLogoutButton(ActionEvent event) {
+
+    }
+
+}
