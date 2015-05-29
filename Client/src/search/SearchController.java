6b72a7d4673ec0d127a5cbf566e904ce6058f067
AHenk <ajhenk0955@eagle.fgcu.edu>
2015-05-29 11:29:22 -0400

    SearchController Skeleton

---
+++ Client/src/search/SearchController.java
@@ -0,0 +1,45 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
+
+public class SearchController {
+
+    @FXML
+    private CheckBox earBox;
+
+    @FXML
+    private Button Search1_goButton;
+
+    @FXML
+    private CheckBox noseBox;
+
+    @FXML
+    private TextField searchTerm;
+
+    @FXML
+    private Button backButton;
+
+    @FXML
+    private CheckBox medicareBox;
+
+    @FXML
+    private CheckBox vaBox;
+
+    @FXML
+    private CheckBox eyeBox;
+
+    @FXML
+    private CheckBox insuranceBox;
+
+    @FXML
+    void search1_buttons(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleBackButton(ActionEvent event) {
+
+    }
+
+}
