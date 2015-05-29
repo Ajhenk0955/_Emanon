c3ae079338f5e043fb78324dc0c00e55ac5e8881
AHenk <ajhenk0955@eagle.fgcu.edu>
2015-05-29 11:36:37 -0400

    LoginController Skeleton
    May rebuild login to stackpane. TBD

---
+++ LoginController.java
@@ -0,0 +1,43 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+
+    @FXML
+    private TextField login_Username;
+
+    @FXML
+    private Label login_Label;
+
+    @FXML
+    private Button signUpButton;
+
+    @FXML
+    private Button login_Button;
+
+    @FXML
+    private PasswordField login_Password;
+
+    @FXML
+    private Hyperlink login_Hyperlink;
+
+    @FXML
+    void handleLoginHyperlink(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleSignUpButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleLoginButton(ActionEvent event) {
+
+    }
+
+}
